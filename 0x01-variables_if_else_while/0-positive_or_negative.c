22e7a22791a5d1e6dadb178e8516dd9a8ddeda58
Ifeanyichukwu Cornelius Ikechukwu <ifeanyichukwu.c.ikechukwu@gmail.com>
2023-02-19 15:59:34 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,7 @@
 #include <time.h>
 /* more headers goes there */
 
+
 /* betty style doc for function main goes there */
 
 int main(void)
@@ -17,6 +18,5 @@ int main(void)
 		printf("%d is negative\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
-
 	return (0);
 }

82cecd044ef59d6f28a804732c4af629bd909690
Ifeanyichukwu Cornelius Ikechukwu <ifeanyichukwu.c.ikechukwu@gmail.com>
2023-02-20 16:02:18 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,16 +9,16 @@
 * Return: Always 0 (Success)
 */
 
+
 int main(void)
 
 {
-
-	int i;
-
-	for (i = 0; i < 10; i++)
-	printf("%d", i);
-	printf("\n");
-
+	int d;
+	
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+	
 	return (0);
 
 
